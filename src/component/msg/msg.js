59b684326c2b05669e59d7f7707a334fe95332fc
wujian <wujian@acfun.tv>
2018-01-03 18:34:08 +0800

    msg list update

--- src/component/msg/msg.js
+++ src/component/msg/msg.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {List} from 'antd-mobile'
+import {List, Badge} from 'antd-mobile'
 
 
 @connect(
@@ -21,18 +21,28 @@ class Msg extends React.Component{
       msgGroup[v.chatid].push(v)
     })
     console.log('msgGroup',msgGroup)
-    const chatList = Object.values(msgGroup)
+    const chatList = Object.values(msgGroup).sort((a,b)=>{
+      const a_last = this.getLast(a)
+      const b_last = this.getLast(b)
+      return b_last - a_last
+    })
     return (
       <div>
           {chatList.map(v=>{
             const lastItem = this.getLast(v)
             const targetId = v[0].from === userid?v[0].to:v[0].from
+            const unreadNum = v.filter(v=>!v.read&&v.to===userid).length
             const name = userinfo[targetId]?userinfo[targetId].name:''
             const avatar = userinfo[targetId]?userinfo[targetId].avatar:''
             return(
               <List  key={lastItem._id}>
                 <Item
+                  extra={<Badge text={unreadNum}></Badge>}
                   thumb={require(`../img/${userinfo[targetId].avatar}.png`)}
+                  arrow="horizontal"
+                  onClick={()=>{
+                    this.props.history.push(`/chat/${targetId}`)
+                  }}
                 >
                   {lastItem.content}
                 <Brief>{name}</Brief>
